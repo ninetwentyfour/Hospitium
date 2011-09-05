2c159c12bc92908055d031cc883410a96b4285a7
Travis Berry <travisberry@travisberry.com>
2011-09-04 18:59:41 -0600

    forced sign in redirect to admin dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  protect_from_forgery # Turn on request forgery protection. Bear in mind that only non-GET, HTML/JavaScript requests are checked.
+  
+  # redirect to /admin after all logins
+  def after_sign_in_path_for(resource_or_scope)
+    '/admin'
+  end
 end
