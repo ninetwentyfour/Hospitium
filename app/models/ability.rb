146a4505037641e1a37fff7ca16b13c796fb1f47
Travis Berry <travisberry@travisberry.com>
2011-09-23 20:28:53 -0600

    updated permissions for admin

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -35,40 +35,56 @@ class Ability
       elsif user.role? :admin
         can :access, :rails_admin
         #can :manage, :all
-        can :manage, AdoptionContact, :animal => { :organization_id => user.organization_ids }
+        #can :manage, AdoptionContact, :animal => { :organization_id => user.organization_ids }
+        can :read, AdoptionContact, :animal => { :organization_id => user.organization_ids }
+        can :create, AdoptionContact
+        can :update, AdoptionContact, :animal => { :organization_id => user.organization_ids }
+        can :destroy, AdoptionContact,, :animal => { :organization_id => user.organization_ids }
         #can :manage, AdoptionContact
-        can :manage, Animal, :organization => { :id => user.organization_ids }
+        #can :manage, Animal, :organization => { :id => user.organization_ids }
+        can :read, Animal, :organization => { :id => user.organization_ids }
+        can :create, Animal
+        can :update, Animal, :organization => { :id => user.organization_ids }
+        can :destroy, Animal, :organization => { :id => user.organization_ids }
         #can :manage, AnimalColor, :organization => { :id => user.organization_ids }
+        can :read, AnimalColor, :organization => { :id => user.organization_ids }
         can :create, AnimalColor
         can :update, AnimalColor, :organization => { :id => user.organization_ids }
         can :destroy, AnimalColor, :organization => { :id => user.organization_ids }
         #can :manage, AnimalWeight, :animal => { :organization_id => user.organization_ids }
+        can :read, AnimalWeight, :animal => { :organization_id => user.organization_ids }
         can :create, AnimalWeight
         can :update, AnimalWeight, :animal => { :organization_id => user.organization_ids }
         can :destroy, AnimalWeight, :animal => { :organization_id => user.organization_ids }
+        #
         can :read, Organization, :id => user.organization_ids
         can :create, Organization
         can :update, Organization, :id => user.organization_ids
         can :destroy, Organization, :id => user.organization_ids
         #can :manage, RelinquishmentContact, :animal => { :organization_id => user.organization_ids }
+        can :read, RelinquishmentContact, :animal => { :organization_id => user.organization_ids }
         can :create, RelinquishmentContact
         can :update, RelinquishmentContact, :animal => { :organization_id => user.organization_ids }
         can :destroy, RelinquishmentContact, :animal => { :organization_id => user.organization_ids }
         #can :manage, Role, :organization => { :id => user.organization_ids }
         #can :manage, Shelter, :organization => { :id => user.organization_ids }
+        can :read, Shelter, :organization => { :id => user.organization_ids }
         can :create, Shelter
         can :update, Shelter, :organization => { :id => user.organization_ids }
         can :destroy, Shelter, :organization => { :id => user.organization_ids }
         #can :manage, Species, :organization => { :id => user.organization_ids }
+        can :read, Species, :organization => { :id => user.organization_ids }
         can :create, Species
         can :update, Species, :organization => { :id => user.organization_ids }
         can :destroy, Species, :organization => { :id => user.organization_ids }
         can :manage, User, :id => user.id
         #can :manage, VetContact, :organization => { :id => user.organization_ids }
+        can :read, VetContact, :organization => { :id => user.organization_ids }
         can :create, VetContact
         can :update, VetContact, :organization => { :id => user.organization_ids }
         can :destroy, VetContact, :organization => { :id => user.organization_ids }
         #can :manage, VolunteerContact, :organization => { :id => user.organization_ids }
+        can :read, VolunteerContact, :organization => { :id => user.organization_ids }
         can :create, VolunteerContact
         can :update, VolunteerContact, :organization => { :id => user.organization_ids }
         can :destroy, VolunteerContact, :organization => { :id => user.organization_ids }
