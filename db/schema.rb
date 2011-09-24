66841c0630b05b35342cbc32d33b962c1605f2f0
Travis Berry <travisberry@travisberry.com>
2011-09-24 14:53:51 -0600

    added confirmable to model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110924183150) do
+ActiveRecord::Schema.define(:version => 20110924205206) do
 
   create_table "adoption_contacts", :force => true do |t|
     t.string   "first_name"
@@ -169,8 +169,12 @@ ActiveRecord::Schema.define(:version => 20110924183150) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "username"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
