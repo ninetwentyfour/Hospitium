b5faa58e17722e86f7564b024e7afea9002958e7
Travis Berry <travisberry@travisberry.com>
2011-09-24 18:59:50 -0600

    trying to store session in memcache

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
 #AnimalTracker::Application.config.session_store :cookie_store, :key => '_animal_tracker_session'
-AnimalTracker::Application.config.session_store :active_record_store
+#AnimalTracker::Application.config.session_store :active_record_store
+AnimalTracker::Application.config.session_store :mem_cache_store
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
