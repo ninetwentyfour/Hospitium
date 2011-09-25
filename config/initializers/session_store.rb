aa2a39d6b3f4df7589da3ebd6f3291920d67b3c9
Travis Berry <travisberry@travisberry.com>
2011-09-24 19:04:30 -0600

    using dali store for session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 #AnimalTracker::Application.config.session_store :cookie_store, :key => '_animal_tracker_session'
-AnimalTracker::Application.config.session_store :active_record_store
+AnimalTracker::Application.config.session_store :dalli_store
 #AnimalTracker::Application.config.session_store :mem_cache_store
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
