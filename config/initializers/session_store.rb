7c2bc7cdc81c88e019ca3de248e2d8157c40a8e5
Travis Berry <travisberry@travisberry.com>
2011-09-24 18:55:54 -0600

    changed session to db store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-AnimalTracker::Application.config.session_store :cookie_store, :key => '_animal_tracker_session'
-
+#AnimalTracker::Application.config.session_store :cookie_store, :key => '_animal_tracker_session'
+AnimalTracker::Application.config.session_store :active_record_store
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110924233059) do
+ActiveRecord::Schema.define(:version => 20110925005449) do
 
   create_table "adoption_contacts", :force => true do |t|
     t.string   "first_name"
@@ -144,6 +144,16 @@ ActiveRecord::Schema.define(:version => 20110924233059) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "shelters", :force => true do |t|
     t.string   "name"
