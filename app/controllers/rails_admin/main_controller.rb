4318b97370e6489242b6f833c577584741bd1776
Travis Berry <travisberry@travisberry.com>
2011-09-24 19:55:01 -0600

    removed from rails admin

--- app/controllers/rails_admin/main_controller.rb
+++ app/controllers/rails_admin/main_controller.rb
@@ -2,8 +2,7 @@ module RailsAdmin
 
   class MainController < RailsAdmin::ApplicationController
     include ActionView::Helpers::TextHelper
-    cache_sweeper :animal_sweeper
-    cache_sweeper :organization_sweeper
+
 
     layout "rails_admin/main"
 
