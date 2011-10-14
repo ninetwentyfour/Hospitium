8b4d4698bf15a8b109f248731f02a5b8b88786f9
Travis Berry <travisberry@travisberry.com>
2011-10-13 20:31:37 -0600

    changed order on posts index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.xml
   def index
-    @posts = Post.paginate(:page => params[:page], :per_page => 10)
+    @posts = Post.paginate(:page => params[:page], :per_page => 10, :order => "updated_at DESC")
     #@posts = Post.all
 
     respond_to do |format|
