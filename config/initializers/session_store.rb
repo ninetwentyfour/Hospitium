0c0934a8a758e59f9e6ca2d78c4a608284c19ea0
Travis Berry <travisberry@travisberry.com>
2011-10-14 17:58:29 -0600

    added if to session store to change in env

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,9 +3,15 @@
 #AnimalTracker::Application.config.session_store :cookie_store, :key => '_animal_tracker_session'
 #require 'action_dispatch/middleware/session/dalli_store'
 #AnimalTracker::Application.config.session_store :dalli_store, :namespace => 'sessions', :key => '_foundation_session', :expire_after => 30.minutes
-AnimalTracker::Application.config.session_store :active_record_store
+#AnimalTracker::Application.config.session_store :active_record_store
 #AnimalTracker::Application.config.session_store :mem_cache_store
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
 # AnimalTracker::Application.config.session_store :active_record_store
+if RAILS_ENV == "production"
+  require 'action_dispatch/middleware/session/dalli_store'
+  AnimalTracker::Application.config.session_store :dalli_store, :namespace => 'sessions', :key => '_foundation_session', :expire_after => 30.minutes
+else
+  AnimalTracker::Application.config.session_store :active_record_store
+end
