fab3253bf1647cbbcae9054fcd58a31926300fcb
Travis Berry <travisberry@travisberry.com>
2011-10-13 20:05:22 -0600

    turned off posts rails admin settings

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,21 +2,21 @@ class Post < ActiveRecord::Base
   has_paper_trail
   
   # settings for rails admin views
-  # rails_admin do
-  #   create do
-  #     field :title
-  #     field :author
-  #     field :content, :text do
-  #       ckeditor true
-  #     end
-  #   end
-  #   edit do
-  #     field :title
-  #     field :author
-  #     field :content, :text do
-  #       ckeditor true
-  #     end
-  #   end
-  # end
+  rails_admin do
+    create do
+      #field :title
+      #field :author
+      field :content, :text do
+        ckeditor true
+      end
+    end
+    edit do
+      #field :title
+      #field :author
+      field :content, :text do
+        ckeditor true
+      end
+    end
+  end
   
 end
