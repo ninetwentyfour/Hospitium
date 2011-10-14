d1726bba0783c5263e101d17bff8924091c8957e
Travis Berry <travisberry@travisberry.com>
2011-10-13 22:10:01 -0600

    updated url to redirect to hospitium.co

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_filter :ensure_domain
   protect_from_forgery # Turn on request forgery protection. Bear in mind that only non-GET, HTML/JavaScript requests are checked.
   
   # redirect to /admin after all logins
@@ -9,5 +10,14 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     exception.default_message = "Default error message"
     redirect_to root_url, :notice => "That URL is prohibited"
+  end
+
+  APP_DOMAIN = 'hospitium.co'
+
+  def ensure_domain
+    if request.env['HTTP_HOST'] != APP_DOMAIN
+      # HTTP 301 is a "permanent" redirect
+      redirect_to "http://#{APP_DOMAIN}", :status => 301
+    end
   end
 end
