212ce90f39153fbc1e01c487823c3397564757cd
Travis Berry <travisberry@travisberry.com>
2011-10-30 13:00:17 -0600

    testing animal show cache

--- app/controllers/rails_admin/main_controller.rb
+++ app/controllers/rails_admin/main_controller.rb
@@ -38,11 +38,11 @@ module RailsAdmin
       
       
       #generate the animal percentages for the dashboard
-      @final_status_hash = Rails.cache.fetch("animal_status_hash_user_#{current_user.id}", :expires_in => 1.minutes) do
+      @final_status_hash = Rails.cache.fetch("animal_status_hash_user_#{current_user.organization.id}", :expires_in => 1.minutes) do
         Report.animals_by_status(current_user.organization.id)
       end
       #@final_status_hash = Report.animals_by_status(current_user.organization.id)
-      @final_species_hash = Rails.cache.fetch("animal_species_hash_user_#{current_user.id}", :expires_in => 1.minutes) do
+      @final_species_hash = Rails.cache.fetch("animal_species_hash_user_#{current_user.organization.id}", :expires_in => 1.minutes) do
         Report.animals_by_species(current_user.organization.id)
       end
       #@final_species_hash = Report.animals_by_species(current_user.organization.id)
@@ -67,24 +67,24 @@ module RailsAdmin
       @max = 0
       @abstract_models.each do |t|
         unless t.model.model_name == "Organization" or t.model.model_name == "Post" or t.model.model_name == "Role" or t.model.model_name == "SpayNeuter" or t.model.model_name == "AnimalSex"  or t.model.model_name == "Notification"
-          current_count = Rails.cache.fetch("dashboard_top_graph_currentcount_#{t.model.model_name}_user_#{current_user.id}", :expires_in => 2.minutes) do
+          current_count = Rails.cache.fetch("dashboard_top_graph_currentcount_#{t.model.model_name}_user_#{current_user.organization.id}", :expires_in => 2.minutes) do
             t.count(:conditions => {:organization_id => current_user.organization.id})
           end
           #current_count = t.count(:conditions => {:organization_id => current_user.organization.id})
           @max = current_count > @max ? current_count : @max
           @count[t.pretty_name] = current_count
-          @most_recent_changes[t.pretty_name] = Rails.cache.fetch("dashboard_top_graph_recentchanges_#{t.model.model_name}_user_#{current_user.id}", :expires_in => 2.minutes) do
+          @most_recent_changes[t.pretty_name] = Rails.cache.fetch("dashboard_top_graph_recentchanges_#{t.model.model_name}_user_#{current_user.organization.id}", :expires_in => 2.minutes) do
             t.model.order("updated_at desc").first.try(:updated_at) rescue nil
           end
           #@most_recent_changes[t.pretty_name] = t.model.order("updated_at desc").first.try(:updated_at) rescue nil
         else
-          current_count = Rails.cache.fetch("dashboard_top_graph_currentcount2_#{t.model.model_name}_user_#{current_user.id}", :expires_in => 2.minutes) do
+          current_count = Rails.cache.fetch("dashboard_top_graph_currentcount2_#{t.model.model_name}_user_#{current_user.organization.id}", :expires_in => 2.minutes) do
             t.count(:conditions => {:id => current_user.organization.id})
           end
           #current_count = t.count(:conditions => {:id => current_user.organization.id})
           @max = current_count > @max ? current_count : @max
           @count[t.pretty_name] = current_count
-          @most_recent_changes[t.pretty_name] = Rails.cache.fetch("dashboard_top_graph_recentchanges2_#{t.model.model_name}_user_#{current_user.id}", :expires_in => 2.minutes) do
+          @most_recent_changes[t.pretty_name] = Rails.cache.fetch("dashboard_top_graph_recentchanges2_#{t.model.model_name}_user_#{current_user.organization.id}", :expires_in => 2.minutes) do
             t.model.order("updated_at desc").first.try(:updated_at) rescue nil
           end
           #@most_recent_changes[t.pretty_name] = t.model.order("updated_at desc").first.try(:updated_at) rescue nil
@@ -194,7 +194,10 @@ module RailsAdmin
       @authorization_adapter.authorize(:show, @abstract_model, @object) if @authorization_adapter
       @page_name = t("admin.show.page_name", :name => @model_config.label.downcase)
       if @model_config.label.downcase == 'animal'
-        @animal_weights = AnimalWeight.find(:all, :conditions => {:animal_id => @object.id}, :order => "date_of_weight ASC")
+        @animal_weights = Rails.cache.fetch("animal_weights_hash_user_#{current_user.organization.id}", :expires_in => 1.minutes) do
+          AnimalWeight.find(:all, :conditions => {:animal_id => @object.id}, :order => "date_of_weight ASC")
+        end
+        #@animal_weights = AnimalWeight.find(:all, :conditions => {:animal_id => @object.id}, :order => "date_of_weight ASC")
       else
         @animal_weights = ''
       end
