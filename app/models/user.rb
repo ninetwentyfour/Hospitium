c3ad9e32756d09c61a02edd5c5ff49cc8cf3edd8
Travis Berry <travisberry@travisberry.com>
2011-10-30 13:41:23 -0600

    testing animal show cache

--- app/models/user.rb
+++ app/models/user.rb
@@ -90,10 +90,10 @@ class User < ActiveRecord::Base
   
   def role?(role)
     #self.id
-    @roles = Rails.cache.fetch("role_for_user_#{self.id}", :expires_in => 5.minutes) do
-      self.roles.find_by_name(role.to_s.camelize)
+    Rails.cache.fetch("role_for_user_#{self.id}", :expires_in => 5.minutes) do
+      return !!self.roles.find_by_name(role.to_s.camelize)
     end
-    return !!@roles
+    #return !!@roles
       #return !!self.roles.find_by_name(role.to_s.camelize)
   end
   
