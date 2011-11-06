ed33d969367c8a7b2494eaec25869e527da84836
Travis Berry <travisberry@travisberry.com>
2011-11-06 13:27:41 -0700

    cache key dashboard

--- app/controllers/rails_admin/main_controller.rb
+++ app/controllers/rails_admin/main_controller.rb
@@ -43,7 +43,7 @@ module RailsAdmin
       
       #generate the animal percentages for the dashboard
       @animals_count = Animal.count(:conditions => {:organization_id => current_user.organization_id}) 
-      @animal_update = Animal.order("updated_at desc").first.try(:updated_at)
+      @animal_update = Animal.order("updated_at desc").first(:conditions => {:organization_id => current_user.organization_id}).try(:updated_at)
       @final_status_hash = Rails.cache.fetch("animal_status_hash_user_#{current_user.organization_id}_#{@animals_count}_#{@animal_update}") do
         Report.animals_by_status(current_user.organization_id)
       end
