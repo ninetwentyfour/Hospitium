726cb7032a16396f74aff1b42ddc9dbc8efe6c96
Travis Berry <travisberry@travisberry.com>
2011-11-06 13:01:44 -0700

    added object cache back on list

--- app/controllers/rails_admin/main_controller.rb
+++ app/controllers/rails_admin/main_controller.rb
@@ -112,10 +112,10 @@ module RailsAdmin
       @page_name = t("admin.list.select", :name => @model_config.label.downcase)
       @countz = @abstract_model.count
       @updated_at = @abstract_model.model.order("updated_at desc").first.try(:updated_at)
-      #@objects, @current_page, @page_count, @record_count = Rails.cache.fetch("#{params[:model_name]}_t_user_#{current_user.id}_#{params[:page]}_#{params[:sort]}_#{params[:sort_reverse]}_#{params[:filters]}_#{params[:set]}_#{@countz}_#{@updated_at}") do
-      #   list_entries
-      #end
-      @objects, @current_page, @page_count, @record_count = list_entries
+      @objects, @current_page, @page_count, @record_count = Rails.cache.fetch("#{params[:model_name]}_t_user_#{current_user.id}_#{params[:page]}_#{params[:sort]}_#{params[:sort_reverse]}_#{params[:filters]}_#{params[:set]}_#{@countz}_#{@updated_at}") do
+         list_entries
+      end
+      #@objects, @current_page, @page_count, @record_count = list_entries
       @schema ||= { :only => @model_config.list.visible_fields.map {|f| f.name } }
 
       respond_to do |format|
