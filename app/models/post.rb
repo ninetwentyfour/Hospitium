a4fb2596b1c0b0ccd341d729430616caffd6ee3f
Travis Berry <travisberry@travisberry.com>
2011-11-06 20:40:08 -0700

    send blog link after update to twitter

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
   has_paper_trail
+  after_update :send_public_tweet
+  
   def to_params
     "#{id}-#{title.parameterize}"
   end
@@ -30,5 +32,21 @@ class Post < ActiveRecord::Base
   #     field :content
   #   end
   # end
+  def send_public_tweet
+    if self.public == 1
+      account = TwitterAccount.find_by_user_id(1)
+      Twitter.configure do |config|
+        config.consumer_key = TwitterAccount::CONSUMER_KEY
+        config.consumer_secret = TwitterAccount::CONSUMER_SECRET
+        config.oauth_token = account.oauth_token
+        config.oauth_token_secret = account.oauth_token_secret
+      end
+      client = Twitter::Client.new
+      #begin
+      link = TwitterAccount.shorten_link("http://hospitium.co/posts/#{self.id}-#{self.title.parameterize}")
+      client.update("#{self.title} | #{link}")
+      return true
+    end
+  end
   
 end
