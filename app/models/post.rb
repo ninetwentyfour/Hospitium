e201d184e9983a81decd3e025bdd46f3a0154434
Travis Berry <travisberry@travisberry.com>
2012-03-27 18:44:04 -0600

    cleaned up post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,29 +6,13 @@ class Post < ActiveRecord::Base
   
   attr_accessible :author, :title, :content
   
+  #used to create url for posts
   def to_params
     "#{id}-#{title.parameterize}"
   end
   
   def send_public_tweet
     TwitterAccount.send_post_tweet(self)
-    # account = TwitterAccount.find_by_user_id(1)
-    # Twitter.configure do |config|
-    #   config.consumer_key = TwitterAccount::CONSUMER_KEY
-    #   config.consumer_secret = TwitterAccount::CONSUMER_SECRET
-    #   config.oauth_token = account.oauth_token
-    #   config.oauth_token_secret = account.oauth_token_secret
-    # end
-    # client = Twitter::Client.new
-    # link = TwitterAccount.shorten_link("http://hospitium.co/posts/#{self.id}-#{self.title.parameterize}")
-    # begin
-    #    client.update("#{self.title.slice(0, 100)} - #{link}")
-    #    return true
-    #  rescue Twitter::Error
-    #    return true
-    #  rescue Exception
-    #    return true
-    #  end
   end
   
 end
