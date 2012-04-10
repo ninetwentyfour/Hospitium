b168d1f3c74328f4d6f1158a4cd6c6cc2f4f9c21
Travis Berry <travisberry@travisberry.com>
2012-04-09 19:48:18 -0600

    added email on user confirmation

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -5,6 +5,10 @@ class UserObserver < ActiveRecord::Observer
       publish(:update, user)
   end
   
+  def before_save(user)
+    send_user_confirmed_email(user) if user.confirmed_at_changed?
+  end
+  
   
   def publish(type, user)
     Juggernaut.url = ENV['JUGG_URL']
@@ -14,12 +18,16 @@ class UserObserver < ActiveRecord::Observer
       :klass  => user.class.name,
       :record => user.changes
     })
-    # Juggernaut.publish("/observer/animal/index", {
-    #   :id     => animal.uuid, 
-    #   :type   => type, 
-    #   :klass  => animal.class.name,
-    #   :record => animal.changes
-    # })
+  end
+  
+  def send_user_confirmed_email(user)
+    url = "http://sendgrid.com/api/mail.send.json?api_user=#{ENV['SENDGRID_USERNAME']}&api_key=#{ENV['SENDGRID_PASSWORD']}&to=contact@travisberry.com&subject=Hospitium%20-%20User%20Confirmed&text=#{user.username}%20confirmed%20an%20account.%20#{user.email}%20in%20organization%20#{URI::encode(user.organization.name)}&from=contact@hospitium.co"
+    resp = Net::HTTP.get_response(URI.parse(url))
+    data = resp.body
+    result = JSON.parse(data)
+    if result.has_key? 'Error'
+       raise "web service error"
+    end
   end
   
   
