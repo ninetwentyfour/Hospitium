a3fb7a63fb5942f171b3ea08c682ef745c5639b7
Travis Berry <contact@travisberry.com>
2012-10-13 15:15:41 -0600

    turning off all error ignore for testing

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,6 +1,7 @@
 if Rails.env == "production"
   Airbrake.configure do |config|
     config.api_key = ENV['AIRBRAKE_API_KEY']
-    config.ignore << "ActionController::RoutingError"
+    config.ignore_only  = []
+    # config.ignore << "ActionController::RoutingError"
   end
 end
