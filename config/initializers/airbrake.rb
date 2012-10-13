62f0dc4b56ec5d2aa19b231947ac527ac18bda8f
Travis Berry <contact@travisberry.com>
2012-10-13 15:42:25 -0600

    airbrake is working, turning back on travis cucumber tests

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,7 +1,6 @@
 if Rails.env == "production"
   Airbrake.configure do |config|
     config.api_key = ENV['AIRBRAKE_API_KEY']
-    config.ignore_only  = []
-    # config.ignore << "ActionController::RoutingError"
+    config.ignore << "ActionController::RoutingError"
   end
 end
