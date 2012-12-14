3d5ea216aa2c5750aa740a713d456c149a1166e3
Travis Berry <contact@travisberry.com>
2012-12-14 14:02:30 -0700

    added async to airbrake

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -2,5 +2,6 @@ if Rails.env == "production"
   Airbrake.configure do |config|
     config.api_key = ENV['AIRBRAKE_API_KEY']
     config.ignore << "ActionController::RoutingError"
+    config.async = true
   end
 end
