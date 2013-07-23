5897bca80cb16db6c7f059bd886afb25b9b7bece
Travis Berry <contact@travisberry.com>
2013-07-22 19:12:15 -0600

    fix dashboard bug hack

--- app/presenters/admin/home/index_presenter.rb
+++ app/presenters/admin/home/index_presenter.rb
@@ -27,11 +27,11 @@ class Admin::Home::IndexPresenter
     end
   end
   
-  def latest_activity
-    Rails.cache.fetch("latest_activity_hash_user_#{@user.organization_id}_#{@animals_count}_#{@animal_update.to_i}") do
-      Event.find(:all, :conditions => {:organization_id => @user.organization_id}, :limit => 15, :order => "created_at desc")
-    end
-  end
+  # def latest_activity
+  #   Rails.cache.fetch("latest_activity_hash_user_#{@user.organization_id}_#{@animals_count}_#{@animal_update.to_i}") do
+  #     Event.find(:all, :conditions => {:organization_id => @user.organization_id}, :limit => 15, :order => "created_at desc")
+  #   end
+  # end
   
   def public_animals
     Rails.cache.fetch("public_animals_hash_user_#{@user.organization_id}_#{@animals_count}_#{@animal_update.to_i}") do
