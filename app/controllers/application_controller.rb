118a0d191b483f500a6d16daba03fc753dc1e12c
Travis Berry <contact@travisberry.com>
2013-08-07 20:28:18 -0600

    hotfix signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_filter :check_domain
   before_filter :get_notice
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery # Turn on request forgery protection. Bear in mind that only non-GET, HTML/JavaScript requests are checked.
   ensure_security_headers
   
@@ -49,5 +50,11 @@ class ApplicationController < ActionController::Base
   
   def canonical_url(canonical_url)
     @canonical_url = canonical_url
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:organization_name, :username, :email, :owner, :password, :password_confirmation) }
   end
 end
