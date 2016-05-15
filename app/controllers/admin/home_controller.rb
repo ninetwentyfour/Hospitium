65dfc671ae4cc1583ddf23ec827de02688ed289d
Travis Berry <contact@travisberry.com>
2016-05-14 21:21:00 -0600

    thread stats

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -3,14 +3,33 @@ class Admin::HomeController < Admin::ApplicationController
   def index
     @presenter = Admin::Home::IndexPresenter.new(current_user)
 
-    # collect some stats for my dashbaord
-    # Thread.new do
-      $statsd.gauge 'number_users', User.count
-      $statsd.gauge 'number_animals', Animal.count
-      $statsd.gauge 'number_organizations', Organization.count
-      $statsd.gauge 'number_events', PublicActivity::Activity.count
-    # end
+    send_stats
 
     @active_nav = true
+  end
+
+  private
+
+  def send_stats
+    return if [1,2].sample == 1
+    threads = []
+
+    threads << Thread.new do
+      $statsd.gauge 'number_users', User.count
+    end
+
+    threads << Thread.new do
+      $statsd.gauge 'number_animals', Animal.count
+    end
+
+    threads << Thread.new do
+      $statsd.gauge 'number_organizations', Organization.count
+    end
+
+    threads << Thread.new do
+      $statsd.gauge 'number_events', PublicActivity::Activity.count
+    end
+
+    threads.each { |thread| thread.join }
   end
 end
